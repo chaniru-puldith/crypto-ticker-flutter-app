afbb259361fb409b99111eaf21d58289abd54c29
chaniru-puldith <a.a.d.chaniru.p@gmail.com>
2023-08-31 11:52:15 +0530

    First Commit

---
+++ android/app/src/main/java/com/deadhunter/crypto_ticker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.deadhunter.crypto_ticker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'price_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.lightBlue
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
